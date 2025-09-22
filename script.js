093310300d5db9f7fbeaf655e8c5af818ec2cf64
CodingNORVINS <nor1vin.code@gmail.com>
2025-09-22 12:21:52 +0100

    main commit

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
-const menuList = document.querySelector('.menu__list');
 const page = document.querySelector('.page');
-const menuLinks = menuList.querySelectorAll('a');
 
 menuBtn.addEventListener('click', () => {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-	menuList.classList.toggle('active');
 	page.classList.toggle('blur');
-	menuLinks.forEach(link => link.classList.toggle('active'));
-	document.body.style.overflow = menu.classList.contains('active')
-		? 'hidden'
-		: '';
-});
-
-menuLinks.forEach(link => {
-	link.addEventListener('click', () => {
-		menuBtn.classList.remove('active');
-		menu.classList.remove('active');
-		menuList.classList.remove('active');
-		page.classList.remove('blur');
-		menuLinks.forEach(l => l.classList.remove('active'));
-		document.body.style.overflow = '';
-	});
 });
