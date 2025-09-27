599e916cf36b6a659194ad88516247dea6590f0f
CodingNORVINS <nor1vin.code@gmail.com>
2025-09-27 11:13:11 +0100

    hope final commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.burger-menu__button');
+const menu = document.querySelector('.burger-menu');
 const page = document.querySelector('.page');
+const close = document.querySelector('.close-burger-menu');
 
 menuBtn.addEventListener('click', () => {
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+	page.classList.toggle('blur');
+});
+
+close.addEventListener('click', () => {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 	page.classList.toggle('blur');
